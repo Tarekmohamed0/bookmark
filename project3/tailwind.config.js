1b256c1e0e1a423a7e058d46fefcc4f5b4acda5b
tarekmohamed0.tarekmohamed <tarekmohamed0.tarekmohamed19632052110@gmail.com>
2022-05-03 05:55:54 +0200

    greated th main project for test html with tailwindcss

---
+++ project3/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      container:{
+        center:true,
+        screens:{
+          sm:"640px",
+          md:"768px",
+          lg:"1124px",
+          xl:"1124px",
+          "2xl":"1124px",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
